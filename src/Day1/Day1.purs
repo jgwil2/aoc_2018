af9f29ed4a74bc79ea3b0975ad18aabd25cc5507
John Willard <jgwil2@gmail.com>
2019-09-23 23:17:13 -0400

    clean up some code for day 1

--- src/Day1/Day1.purs
+++ src/Day1/Day1.purs
@@ -7,30 +7,38 @@ where
 
 import Prelude
 
-import Data.Array (cons, difference, head, last, reverse, tail, init)
+import Data.Array (cons, difference, head, init, last, reverse, snoc, tail)
 import Data.Foldable (find, foldl, sum)
-import Data.Maybe (Maybe, isJust)
+import Data.Maybe (Maybe(..), isJust)
 import Utilities (fromStringSafe, getSafeArray, getSafeNum, splitTextByNewline)
 
 part1 :: String -> String
 part1 text = show $ sum $ getNums text
 
 part2 :: String -> String
-part2 text = show $ getFirstRepeating (getSafeArray (init (getRunningTotal nums startingTotals)))
+part2 text = show $ f firstRepeating startingTotals nums
   where
     nums = getNums text
     startingTotals = [0]
+    firstRepeating = getFirstRepeating (getRunningTotal nums startingTotals)
 
 getNums :: String -> Array Int
 getNums = map fromStringSafe <<< splitTextByNewline
 
 getRunningTotal :: Array Int -> Array Int -> Array Int
-getRunningTotal [] totals = reverse totals
+getRunningTotal [x] totals = totals
 getRunningTotal xs totals =
-  getRunningTotal (getSafeArray (tail xs)) (cons ((getSafeNum (head xs)) + (getSafeNum (last (reverse totals)))) totals)
+  getRunningTotal (tail' xs) (snoc totals (last' totals + head' xs))
+  where
+    tail' = getSafeArray <<< tail
+    last' = getSafeNum <<< last
+    head' = getSafeNum <<< head
 
--- TODO write a recursive function that checks if `getFirstRepeating` has a
--- value and if it doesn't, cycle through the input again
+f :: Maybe Int -> Array Int -> Array Int -> Int
+f (Just x) runningTotals nums = x
+f Nothing runningTotals nums = f (getFirstRepeating newTotals) newTotals nums
+  where
+    newTotals = getRunningTotal nums runningTotals
 
 getFirstRepeating :: ∀ a. Eq a => Array a -> Maybe a
 getFirstRepeating = head <<< nonUniq
